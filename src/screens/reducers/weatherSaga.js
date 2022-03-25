47c158ddada531108fa3aaafc48787de5e659e65
Terry Jung <100718033+terryjung0244@users.noreply.github.com>
2022-03-24 22:24:16 -0400

    apicall from weatherSagaWatcher

--- src/screens/reducers/weatherSaga.js
+++ src/screens/reducers/weatherSaga.js
@@ -1,12 +1,30 @@
 import { takeLatest, put, select, take } from "redux-saga/effects";
 import { WEATHER_ACTION_CONST } from "../../services/const/actionConst";
 
-const {WEATHER_API_CALL_ACTION} = WEATHER_ACTION_CONST;
+const { WEATHER_API_CALL_ACTION, } = WEATHER_ACTION_CONST;
 
-function* getApiResult(action) {
-  console.log('123')
-};
+const addDelay = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(null);
+    }, 2000)
+  })
+}; 
+
+//d46fe6f3b2e44fb8b52873c21312b71f
+//https://api.weatherbit.io/v2.0/current?city=""&country=""&key=""
+//GET
+// (`https://api.weatherbit.io/v2.0/current?
+//   city=${}&country=${}&key="d46fe6f3b2e44fb8b52873c21312b71f"`)
+
+function* getApiResult (action) {
+  console.log(action.payload)
+  let apiResult = yield fetch(`https://api.weatherbit.io/v2.0/current?city=${action.payload.cityName}&country=${action.payload.country}&key="d46fe6f3b2e44fb8b52873c21312b71f`)
+  apiResult = yield apiResult.json();
+  console.log(apiResult);
+  
+}
 
 export function* weatherSagaWatcher() {
-  yield takeLatest (WEATHER_API_CALL_ACTION, getApiResult);
+  yield takeLatest (WEATHER_API_CALL_ACTION, getApiResult)
 }
